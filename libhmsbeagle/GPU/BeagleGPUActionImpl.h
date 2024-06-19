47de00e16e55a7c930bed658e6d40d7178213a43
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-06-19 15:42:16 -0400

    Move cuda memory wrappers to declaration header.

--- libhmsbeagle/GPU/BeagleGPUActionImpl.h
+++ libhmsbeagle/GPU/BeagleGPUActionImpl.h
@@ -109,6 +109,50 @@ Real normPInf(Real* matrix, int nRows, int nCols, cublasHandle_t cublasH) {
     return std::abs(result);
 }
 
+template <typename T>
+T* cudaDeviceNew(int n)
+{
+    T* result;
+    auto status = cudaMalloc((void**)&result, n*sizeof(T));
+    if (status != cudaSuccess)
+	throw std::runtime_error("cudaMalloc: failed!");
+    return result;
+}
+
+template <typename T>
+void MemcpyHostToDevice(T* dptr, const T* hptr, int n)
+{
+    auto status = cudaMemcpy(dptr, hptr, n*sizeof(T), cudaMemcpyHostToDevice);
+    if (status != cudaSuccess)
+	throw std::runtime_error("cudaMemcpy(Host->Device): failed!");
+}
+
+template <typename T>
+void MemcpyDeviceToDevice(T* dptr, const T* hptr, int n)
+{
+    auto status = cudaMemcpy(dptr, hptr, n*sizeof(T), cudaMemcpyDeviceToDevice);
+    if (status != cudaSuccess)
+	throw std::runtime_error("cudaMemcpy(Host->Device): failed!");
+}
+
+template <typename T>
+void MemcpyDeviceToHost(T* hptr, const T* dptr, int n)
+{
+    auto status = cudaMemcpy(hptr, dptr, n*sizeof(T), cudaMemcpyDeviceToHost);
+    if (status != cudaSuccess)
+	throw std::runtime_error("cudaMemcpy(Device->Host): failed!");
+}
+
+template <typename T>
+std::vector<T> MemcpyDeviceToHostVector(const T* dptr, int n)
+{
+    std::vector<T> host_vec(n);
+    auto status = cudaMemcpy(host_vec.data(), dptr, n*sizeof(T), cudaMemcpyDeviceToHost);
+    if (status != cudaSuccess)
+	throw std::runtime_error("cudaMemcpy(Device->Host): failed!");
+    return host_vec;
+}
+
 template <typename Real>
 using SpMatrix = Eigen::SparseMatrix<Real, Eigen::StorageOptions::RowMajor>;
 
--- libhmsbeagle/GPU/BeagleGPUActionImpl.hpp
+++ libhmsbeagle/GPU/BeagleGPUActionImpl.hpp
@@ -258,50 +258,6 @@ long long BeagleGPUActionImpl<BEAGLE_GPU_GENERIC>::getFlags()
     return flags;
 }
 
-template <typename T>
-T* cudaDeviceNew(int n)
-{
-    T* result;
-    auto status = cudaMalloc((void**)&result, n*sizeof(T));
-    if (status != cudaSuccess)
-	throw std::runtime_error("cudaMalloc: failed!");
-    return result;
-}
-
-template <typename T>
-void MemcpyHostToDevice(T* dptr, const T* hptr, int n)
-{
-    auto status = cudaMemcpy(dptr, hptr, n*sizeof(T), cudaMemcpyHostToDevice);
-    if (status != cudaSuccess)
-	throw std::runtime_error("cudaMemcpy(Host->Device): failed!");
-}
-
-template <typename T>
-void MemcpyDeviceToDevice(T* dptr, const T* hptr, int n)
-{
-    auto status = cudaMemcpy(dptr, hptr, n*sizeof(T), cudaMemcpyDeviceToDevice);
-    if (status != cudaSuccess)
-	throw std::runtime_error("cudaMemcpy(Host->Device): failed!");
-}
-
-template <typename T>
-void MemcpyDeviceToHost(T* hptr, const T* dptr, int n)
-{
-    auto status = cudaMemcpy(hptr, dptr, n*sizeof(T), cudaMemcpyDeviceToHost);
-    if (status != cudaSuccess)
-	throw std::runtime_error("cudaMemcpy(Device->Host): failed!");
-}
-
-template <typename T>
-std::vector<T> MemcpyDeviceToHostVector(const T* dptr, int n)
-{
-    std::vector<T> host_vec(n);
-    auto status = cudaMemcpy(host_vec.data(), dptr, n*sizeof(T), cudaMemcpyDeviceToHost);
-    if (status != cudaSuccess)
-	throw std::runtime_error("cudaMemcpy(Device->Host): failed!");
-    return host_vec;
-}
-
 // Create an io-manipulator so that we can write std::cerr<<byRow(D)<<"\n";
 template <typename Real>
 class byRowDense
