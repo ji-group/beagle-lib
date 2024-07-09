c0a41e75c8d8bc5553daa78c0125acb7d2fc9833
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-07-09 13:27:58 -0400

    Add question.

--- libhmsbeagle/GPU/cuda_ops.cu
+++ libhmsbeagle/GPU/cuda_ops.cu
@@ -7,6 +7,7 @@ void cuda_log_vector(double* v, int length)
 {
     thrust::device_ptr<double> vdptr = thrust::device_pointer_cast<double>(v);
 
+    // QUESTION: How slow is allocating the device memory here?
     thrust::device_vector<double> V(vdptr, vdptr + length);
 
     thrust::transform(V.begin(), V.end(), vdptr, [] __device__ (double x) {return log(x);});
@@ -16,6 +17,7 @@ void cuda_log_vector(float* v, int length)
 {
     thrust::device_ptr<float> vdptr = thrust::device_pointer_cast<float>(v);
 
+    // QUESTION: How slow is allocating the device memory here?
     thrust::device_vector<float> V(vdptr, vdptr + length);
 
     thrust::transform(V.begin(), V.end(), vdptr, [] __device__ (float x) {return log(x);});
