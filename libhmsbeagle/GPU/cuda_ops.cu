1ff16d9bafbc84a07de6885f56b035f84d15459b
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-07-09 12:56:10 -0400

    Simplify cuda_log_vector

--- libhmsbeagle/GPU/cuda_ops.cu
+++ libhmsbeagle/GPU/cuda_ops.cu
@@ -1,19 +1,15 @@
 #include "cuda_ops.h"
 #include <thrust/device_vector.h>
 
+// QUESTION: Why can't we do the transform in-place?
+
 void cuda_log_vector(double* v, int length)
 {
     thrust::device_ptr<double> vdptr = thrust::device_pointer_cast<double>(v);
 
     thrust::device_vector<double> V(vdptr, vdptr + length);
 
-    thrust::device_vector<double> Vout = V;
-
-    thrust::transform(V.begin(), V.end(), Vout.begin(), [] __device__ (double x) {return log(x);});
-
-    double* Vout_ptr = thrust::raw_pointer_cast(&Vout[0]);
-    
-    MemcpyDeviceToDevice<double>(v, Vout_ptr, length);
+    thrust::transform(V.begin(), V.end(), vdptr, [] __device__ (double x) {return log(x);});
 }
 
 void cuda_log_vector(float* v, int length)
@@ -22,12 +18,6 @@ void cuda_log_vector(float* v, int length)
 
     thrust::device_vector<float> V(vdptr, vdptr + length);
 
-    thrust::device_vector<float> Vout = V;
-
-    thrust::transform(V.begin(), V.end(), Vout.begin(), [] __device__ (float x) {return log(x);});
-
-    float* Vout_ptr = thrust::raw_pointer_cast(&Vout[0]);
-    
-    MemcpyDeviceToDevice<float>(v, Vout_ptr, length);
+    thrust::transform(V.begin(), V.end(), vdptr, [] __device__ (float x) {return log(x);});
 }
 
