5aa5c5ee2125621793601ba31e67b06b1f9d5027
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-08-05 15:59:36 -0400

    Also add the REAL=double version.

--- libhmsbeagle/GPU/cuda_ops.cu
+++ libhmsbeagle/GPU/cuda_ops.cu
@@ -204,6 +204,49 @@ void  rescalePartials2(bool scalers_log, int kCategoryCount, int kPaddedPatternC
 		       double* partials, double* scalingFactors, double* cumulativeScalingBuffer, int streamIndex)
 {
     auto start_pattern = thrust::make_counting_iterator<int>(0);
-    auto rescale_pattern = [] __device__ (int pattern) {};
+    auto rescale_pattern = [=] __device__ (int pattern) {
+
+        // FIND_MAX_PARTIALS_X_CPU();
+        int deltaPartialsByState = pattern * kPaddedStateCount;
+        double max = 0;
+        for(int m = 0; m < kCategoryCount; m++)
+        {
+            int deltaPartialsByCategory = m * kPaddedStateCount * kPaddedPatternCount;
+            int deltaPartials = deltaPartialsByCategory + deltaPartialsByState;
+            for(int i = 0; i < kPaddedStateCount; i++) {
+                double iPartial = partials[deltaPartials + i];
+                if (iPartial > max)
+                    max = iPartial;
+            }
+        }
+
+        if (max == 0)
+            max = 1.0;
+
+        if (scalers_log)
+        {
+            double logMax = log(max);
+            scalingFactors[pattern] = logMax;
+            if (cumulativeScalingBuffer != 0)
+                cumulativeScalingBuffer[pattern] += logMax;
+        }
+        else
+        {
+            scalingFactors[pattern] = max;
+            if (cumulativeScalingBuffer != 0)
+                cumulativeScalingBuffer[pattern] += log(max);
+        }
+
+        // SCALE_PARTIALS_X_CPU();
+        for(int m = 0; m < kCategoryCount; m++)
+        {
+            int deltaPartialsByCategory = m * kPaddedStateCount * kPaddedPatternCount;
+            int deltaPartials = deltaPartialsByCategory + deltaPartialsByState;
+            for(int i = 0; i < kPaddedStateCount; i++) {
+                partials[deltaPartials + i] /= max;
+            }
+        }
+    };
+
     thrust::for_each(start_pattern, start_pattern + kPaddedPatternCount, rescale_pattern);
 }
