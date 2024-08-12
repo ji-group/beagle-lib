b3893c404a46a591c8e2ae40c81e55ccf8a8ad17
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-08-12 14:45:10 -0400

    Don't allocate device memory to do thrust::transform

--- libhmsbeagle/GPU/cuda_ops.cu
+++ libhmsbeagle/GPU/cuda_ops.cu
@@ -9,25 +9,18 @@
 #include <thrust/reduce.h>
 #include <thrust/functional.h>
 
-// QUESTION: Why can't we do the transform in-place?
 void cuda_log_vector(double* v, int length)
 {
     thrust::device_ptr<double> vdptr = thrust::device_pointer_cast<double>(v);
 
-    // QUESTION: How slow is allocating the device memory here?
-    thrust::device_vector<double> V(vdptr, vdptr + length);
-
-    thrust::transform(V.begin(), V.end(), vdptr, [] __device__ (double x) {return log(x);});
+    thrust::transform(vdptr, vdptr + length, vdptr, [] __device__ (double x) {return log(x);});
 }
 
 void cuda_log_vector(float* v, int length)
 {
     thrust::device_ptr<float> vdptr = thrust::device_pointer_cast<float>(v);
 
-    // QUESTION: How slow is allocating the device memory here?
-    thrust::device_vector<float> V(vdptr, vdptr + length);
-
-    thrust::transform(V.begin(), V.end(), vdptr, [] __device__ (float x) {return log(x);});
+    thrust::transform(vdptr, vdptr + length, vdptr, [] __device__ (float x) {return log(x);});
 }
 
 template <typename REAL>
